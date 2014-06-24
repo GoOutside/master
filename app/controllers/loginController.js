1c26e84b13fd6975e6c5183183cd6e95a1e21a57
Hailey Petway <petwayh@gmail.com>
2014-06-24 16:51:53 -0700

    adding basic auth, login still not working, all logins accepted

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,13 +1,27 @@
+'use strict';
+
 module.exports = function(app) {
-  app.controller('LoginController', function($scope, $location){
-    $scope.facebook= function(){
-      //insert facebook call here
-    },
-    $scope.twitter= function(){
-      //insert twitter call here
-    },
-    $scope.signin=function(){
-      $location.path('/signin');
+  app.controller('loginController', function($scope, $http, $base64, $cookies, $location){
+    // $scope.facebook = function(){
+    //   //insert facebook call here
+    // };
+    // $scope.twitter = function(){
+    //   //insert twitter call here
+    // };
+    $scope.signin = function(){
+      console.log($scope.user.email + ':' + $scope.user.password);
+      $http.defaults.headers.common['Authentication'] = 'Basic ' + $base64.encode($scope.user.email + ':' + $scope.user.password);
+
+      $http({
+        method: 'GET',
+        url: 'https://localhost:3000/api/v0_0_1/users'
+      }).success(function(data){
+        $cookies.jwt = data.jwt;
+        $location.path('/user');
+      }).error(function(data){
+        console.log(data);
+      });
     };
   });
 };
+
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,5 +1,9 @@
 module.exports = function(app) {
-  app.controller('UserController', function($scope){
-    
+  app.controller('UserController', function($scope, $http, $cookies){
+    $http.defaults.headers.common['jwt'] = $cookies.jwt;
+
+    console.log('UserController Success!!');
+
+
   });
 };

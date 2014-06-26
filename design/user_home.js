f7da258fc97484264cc06cb6ee39e5e1ce5c979c
Max Brockman <maxbrockman453@gmail.com>
2014-06-26 12:02:29 -0700

    getting jquery drop menues working on user home

--- design/user_home.js
+++ design/user_home.js
@@ -2,3 +2,14 @@ $(".menu-link").click(function(){
   $(".user_side_menu").toggleClass("active");
   $(".activity_location").toggleClass("active");
 });
+
+// $('.activity_list_item').on("click", function(event){
+//    event.preventDefault();
+//   $('.panel').slideToggle('fast', 0);
+// });
+
+  $('.activity_list_item').on("click", function(){
+    $(this).find('.panel').slideToggle('fast', 0);
+  });
+
+

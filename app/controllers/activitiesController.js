2a7a57e0376f5b38382c72f6634e9a8aa9ef0496
Jeff Adelman <jeffadelman8@yahoo.com>
2014-07-03 12:06:58 -0700

    added jwt auth to activities controller

--- app/controllers/activitiesController.js
+++ app/controllers/activitiesController.js
@@ -2,6 +2,10 @@
 
 module.exports = function(app) {
   app.controller('activitiesController', function($scope, $http, $cookies, $location){
+    if(!$cookies.jwt){
+      $location.path('/');
+      return false;
+    }
     $scope.hikeMap = function(){
       $location.path('/user');
     }

dc6b4aca799b65c7328d441e41267ce66e69f992
Jeff Adelman <jeffadelman8@yahoo.com>
2014-07-03 12:15:42 -0700

    rebuild

--- client/client.js
+++ client/client.js
@@ -32623,6 +32623,10 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
 
 module.exports = function(app) {
   app.controller('activitiesController', function($scope, $http, $cookies, $location){
+    if(!$cookies.jwt){
+      $location.path('/');
+      return false;
+    }
     $scope.hikeMap = function(){
       $location.path('/user');
     }
